7b7b9483040fbdbe8ff180164c021a2a0f2d966a
vishalyadav247 <104371900+vishalyadav247@users.noreply.github.com>
2022-07-29 03:02:20 +0530

    Portfolio Updated

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("welcome to javascript"); 
+console.log($);
+
+// type effect
+let typed = new Typed(".animated_text", {
+    strings: ["I'm a Web Developer", "I Love to Design UI"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+})
